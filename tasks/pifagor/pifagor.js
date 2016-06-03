65bcf4808e3e359adf651b1781599b143917f960
kihors <kihors@gmail.com>
2016-06-03 11:08:36 +0300

    pifagor

--- tasks/pifagor/pifagor.js
+++ tasks/pifagor/pifagor.js
@@ -1,22 +1,22 @@
 // pifdom.js
-function pifagorTableDOM() {
+function pifagorTable() {
 	var i, j, prod;
 	var main = document.getElementById("main");
 
 	// pifagorTable
-	for (i = 1; i <= 9;i++) {
+	for (i = 1; i <= 9; i++) {
 		var row = document.createElement("div");
 		row.setAttribute("class", "row");
-		for (j = 1; j <= 9;j++) {
+		for (j = 1; j <= 9; j++) {
 			prod = i * j;
 			var cell = document.createElement("span");
 			cell.setAttribute("class", "cell cell-odd");
 			cell.innerHTML = prod;
 			row.appendChild(cell);
 
-			/*if (prod % 2 == 0) {
+			if (prod % 2 == 0) {
 				cell.setAttribute("class", "cell cell-even");
-			}*/
+			}
 
 
 		} // end of j
