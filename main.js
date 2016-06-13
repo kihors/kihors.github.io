2afb390039a5774fbd1e3d0b865975f178b7d317
kihors <kihors@gmail.com>
2016-06-13 16:21:53 +0300

    main.js

--- main.js
+++ main.js
@@ -2,20 +2,27 @@
  * Open Weather API call
 */
 
-(function() {
+function request(CITY_ID) {
+	var URL = "http://api.openweathermap.org/data/2.5/weather?id=";
 	var API_KEY = "d75da5829ee2244d5a25bbb10c1a7318";
-	var CITY_ID = "707471"; // Ivano-Frankivsk
-	var URL = "http://api.openweathermap.org/data/2.5/forecast/city?id=" + CITY_ID + "&appid=" + API_KEY;
+	var REQ_URL = URL + CITY_ID + "&appid=" + API_KEY;
+	var RESP;
 
 	// XHR creation
 	var XHR = new XMLHttpRequest();
-	XHR.open("GET", URL, true);
+	XHR.open("GET", REQ_URL, true);
 	XHR.send();
+
 	XHR.onreadystatechange = function() {
 		if ((XHR.readyState == 4) && (XHR.status == 200))
 		{
-			console.log(XHR.responseText);
+			RESP=JSON.parse(XHR.responseText);
+            
+            contentWeather.innerHTML = "";
+            var elem = document.createElement("div");
+            contentWeather.appendChild(elem);
+            elem.innerHTML += "<h4>" + "<b>" + "Температура повітря" + "</b>" + " " + " " + "<i>" + Math.round(RESP.main.temp-273.15) + "&deg" + "C" + "</i>" + "</h4>";
 		}
 	}
 
-}());
+}
